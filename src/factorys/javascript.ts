0f24de790338b2ed9f9c84d5dedb4128d68658d2
suiyun39 <suiyun39@gmail.com>
2024-02-22 03:50:42 +0800

    refactor: 重命名配置生成器

--- src/factorys/javascript.ts
+++ src/factorys/javascript.ts
@@ -2,10 +2,10 @@ import type { Linter } from 'eslint'
 import globals from "globals";
 
 /**
- * 此处包含了非样式类 javascript 规则
- * 由于 eslint 不再维护样式类规则, 因此使用 eslint stylistic 提供的规则. 维护在 stylistic.ts 中
+ * 此处包含了 javascript 相关规则
+ * 由于 eslint 不再维护样式类规则, 此处样式配置应使用 eslint stylistic 提供的规则
  */
-export function core(): Linter.FlatConfig {
+export function javascript(): Linter.FlatConfig {
   return {
     linterOptions: {
       reportUnusedDisableDirectives: true,
