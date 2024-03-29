5a971972814966fb5d6272da1ae1b8a3232d99b9
suiyun39 <suiyun39@gmail.com>
2024-03-29 20:42:33 +0800

    build: 添加规则可视化工具

---
+++ eslint.view.js
@@ -0,0 +1,15 @@
+import { createRequire } from 'node:module'
+
+const require = createRequire(import.meta.url)
+require('sucrase/register/ts')
+
+const { defineConfig } = require('./src/index.ts')
+
+export default defineConfig({
+  typescript: {
+    project: './tsconfig.json',
+  },
+  solid: true,
+  react: true,
+  node: true,
+})
