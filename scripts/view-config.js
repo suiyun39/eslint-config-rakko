672a7fb2c017ce58aa85cb105ef2c329752a13ac
suiyun39 <suiyun39@gmail.com>
2024-08-02 07:54:46 +0800

    build: 配置检查器更换为官方版本

--- scripts/view-config.js
+++
@@ -1,9 +0,0 @@
-import { spawnSync } from 'node:child_process'
-
-process.env.PORT = '3000'
-process.env.NO_OPEN = 'true'
-process.env.ESLINT_CONFIG = 'eslint.view.js'
-
-spawnSync('pnpm', ['exec', 'eslint-flat-config-viewer'], {
-  stdio: 'inherit',
-})
